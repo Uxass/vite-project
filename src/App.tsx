2f325c56fb04068cc6828e06721d85c7921f7e07
Uxass <babusenko2004@mail.ru>
2023-12-22 21:41:51 +0300

    10examples

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,61 @@ const App = () => {
   const dataSource = [
     {
       key: '1',
-      name: 'Mike',
+      name: 'Иван',
       age: 32,
       address: '10 Downing Street',
     },
     {
       key: '2',
-      name: 'John',
+      name: 'Маргарита',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '3',
+      name: 'Степан',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '4',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '5',
+      name: 'Максимильян',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '6',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '7',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '8',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '9',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '10',
+      name: 'Александр',
       age: 42,
       address: '10 Downing Street',
     },
