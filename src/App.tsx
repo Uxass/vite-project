9353d0a1db890ff2b3440072328c8c437717d21b
Uxass <babusenko2004@mail.ru>
2024-01-07 21:01:19 +0300

    46 и 168 строки, использовал интерфейс

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,47 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
-import Table from 'antd/es/table'
+import Table, { ColumnsType } from 'antd/es/table'
 import { Routes, Route, Link } from 'react-router-dom'
 import Artem from './pages/Artyom'
 import Vlad from './pages/Vlad'
 import { ARTEM_ROUTE, VLAD_ROUTE } from './app/routing/config'
 import MainRouter from './app/routing'
 import Navbar from './components/navbar'
+import { Button } from 'antd'
+import { SubmitHandler, useForm } from "react-hook-form";
 const App = () => {
   useEffect(()=>{
     console.log(1)
   },[])
-  const dataSource = [
+  const {
+    register, // метод для регистрации вашего инпута, для дальнейшей работы с ним
+    handleSubmit, // метод для получения данных формы, если валидация прошла успешна
+    formState: {errors, isValid}, // errors - список ошибок валидации для всех полей формы
+    reset // метод для очистки полей формы
+} = useForm<IMyForm>({
+    mode: "onBlur", // парметр onBlur - отвечает за запуск валидации при не активном состоянии поля
+})
+interface IMyForm {
+  name: string;
+  age: number;
+  country: string;
+}
+const saveElement: SubmitHandler<IMyForm> = data => {
+  // здесь мы передаём новый массив, который содержит все старые элементы и новый
+  // ...prev - мы получаем все элементы текущего стэйте (с помощью spread оператора)
+      setTasks((prev) => [...prev, data])
+      reset();
+  }
+const [tasks, setTasks] = useState<IMyForm[]>([])
+  const [page, setPage] = useState<number>(1);
+  interface DataType {
+    key: string;
+    name: string;
+    age: number;
+    address: string;
+  }
+  const dataSource: DataType[] = [
     {
       key: '1',
       name: 'Иван',
@@ -75,9 +104,68 @@ const App = () => {
       age: 42,
       address: '10 Downing Street',
     },
+    {
+      key: '11',
+      name: 'Иван',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '12',
+      name: 'Маргарита',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '13',
+      name: 'Степан',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '14',
+      name: 'Иван',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '15',
+      name: 'Максимильянч',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '16',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '17',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '18',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '19',
+      name: 'Александр',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '20',
+      name: 'Иван',
+      age: 42,
+      address: '10 Downing Street',
+    },
   ];
-  
-  const columns = [
+  const columns: ColumnsType<DataType> = [
     {
       title: 'Name',
       dataIndex: 'name',
@@ -94,20 +182,11 @@ const App = () => {
       key: 'address',
     },
   ];
-  
-  
-   interface DataType {
-     key: string;
-     name: string;
-     age: number;
-     address: string;
-     tags: string[];
-   }
-
   return (
-    <>
-    <Navbar/>
-    <Table dataSource={dataSource} columns={columns} />;
+    <><Navbar/>
+    <Table dataSource={dataSource} columns={columns} pagination={false}/>;
+    <Button onClick={() => setPage(page - 1)}>Назад</Button>
+    <Button onClick={() => setPage(page + 1)}>Вперед</Button>
     {/* <Artem/>
     <Vlad/>    */}
     <Link to={ARTEM_ROUTE + '/1'}>Открыть Артема - v1</Link>
@@ -120,7 +199,48 @@ const App = () => {
     <Route path='artem' element={<Artem/>} />
     <Route path='vlad' element={<Vlad/>} />
     </Routes> */}
-    
+    <form onSubmit={handleSubmit(saveElement)}>
+      <input
+          {...register('name', {
+                  required: "Поле обязательно для заполнения",
+                  minLength: {
+                      value: 7,
+                      message: "Нужно больше символов - Ваше имя"
+                  }
+              }
+        )}
+      />
+      <div>{errors.name?.message}</div>
+      <input 
+          {...register('age', {
+                  required: "Поле обязательно для заполнения",
+                  minLength: {
+                      value: 10,
+                      message: "Нужно больше символов"
+                  }
+              }
+          )}
+      />
+      <input 
+          {...register('country', {
+                  required: "Поле обязательно для заполнения",
+                  minLength: {
+                      value: 3,
+                      message: "Нужно больше символов"
+                  }
+              }
+          )}
+      />
+      <div>{errors.country?.message}</div>
+      <button disabled={!isValid} type="submit">Сохранить</button>
+    </form>
+    {
+      tasks.map((task) => 
+          <p>
+              {task.name} - {task.age} - {task.country}
+          </p>
+      )
+    }
     </>
   )
 
@@ -133,4 +253,6 @@ const App = () => {
 
 
 
-export default App
+export default App;
+
+
